24965553c3e749ad2958dfd5784a8f055ebaa194
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2019-05-13 15:53:28 +0200

    modify script for bash (sh) not zsh

--- lib/color_logger.sh
+++ lib/color_logger.sh
@@ -1,57 +1,57 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
-typeset -A text_colors background_colors emphasis
-typeset -a formatting
+declare -A text_colors background_colors emphasis
+declare -a formatting
 
 escape="\033[";
 reset="${escape}0m";
 
 text_colors=(
-  default          "39"
-  black            "30"
-  red              "31"
-  green            "32"
-  yellow           "33"
-  blue             "34"
-  purple           "35"
-  cyan             "36"
-  white            "37"
-  bright_red       "91"
-  bright_green     "92"
-  bright_yellow    "93"
-  bright_blue      "94"
-  bright_purple    "95"
-  bright_cyan      "96"
-  bright_white     "97"
+  ["default"]="39"
+  ["black"]="30"
+  ["red"]="31"
+  ["green"]="32"
+  ["yellow"]="33"
+  ["blue"]="34"
+  ["purple"]="35"
+  ["cyan"]="36"
+  ["white"]="37"
+  ["bright_red"]="91"
+  ["bright_green"]="92"
+  ["bright_yellow"]="93"
+  ["bright_blue"]="94"
+  ["bright_purple"]="95"
+  ["bright_cyan"]="96"
+  ["bright_white"]="97"
 )
 
 background_colors=(
-  default          "49"
-  black            "40"
-  red              "41"
-  green            "42"
-  yellow           "43"
-  blue             "44"
-  purple           "45"
-  cyan             "46"
-  white            "47"
-  bright_black     "100"
-  bright_red       "101"
-  bright_green     "102"
-  bright_yellow    "103"
-  bright_blue      "104"
-  bright_purple    "105"
-  bright_cyan      "106"
-  bright_white     "107"
+  ["default"]="49"
+  ["black"]="40"
+  ["red"]="41"
+  ["green"]="42"
+  ["yellow"]="43"
+  ["blue"]="44"
+  ["purple"]="45"
+  ["cyan"]="46"
+  ["white"]="47"
+  ["bright_black"]="100"
+  ["bright_red"]="101"
+  ["bright_green"]="102"
+  ["bright_yellow"]="103"
+  ["bright_blue"]="104"
+  ["bright_purple"]="105"
+  ["bright_cyan"]="106"
+  ["bright_white"]="107"
 )
 
 emphasis=(
-  default       "0"
-  bold          "1"
-  italics       "3"
-  underline     "4"
-  blink         "5"
-  reverse       "7"
+  ["default"]="0"
+  ["bold"]="1"
+  ["italics"]="3"
+  ["underline"]="4"
+  ["blink"]="5"
+  ["reverse"]="7"
 )
 
 
