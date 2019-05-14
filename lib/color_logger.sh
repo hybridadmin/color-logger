c36f933828c54811d22d2b03ed34fce71bcccb91
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2019-05-14 15:49:21 +0200

    updated for regular shell - sh

--- lib/color_logger.sh
+++ lib/color_logger.sh
@@ -6,119 +6,114 @@ declare -a formatting
 escape="\033[";
 reset="${escape}0m";
 
-text_colors=(
-  ["default"]="39"
-  ["black"]="30"
-  ["red"]="31"
-  ["green"]="32"
-  ["yellow"]="33"
-  ["blue"]="34"
-  ["purple"]="35"
-  ["cyan"]="36"
-  ["white"]="37"
-  ["bright_red"]="91"
-  ["bright_green"]="92"
-  ["bright_yellow"]="93"
-  ["bright_blue"]="94"
-  ["bright_purple"]="95"
-  ["bright_cyan"]="96"
-  ["bright_white"]="97"
-)
+text_colors[default]=39
+text_colors[black]=30
+text_colors[red]=31
+text_colors[green]=32
+text_colors[yellow]=33
+text_colors[blue]=34
+text_colors[purple]=35
+text_colors[cyan]=36
+text_colors[white]=37
+text_colors[bright_red]=91
+text_colors[bright_green]=92
+text_colors[bright_yellow]=93
+text_colors[bright_blue]=94
+text_colors[bright_purple]=95
+text_colors[bright_cyan]=96
+text_colors[bright_white]=97
 
-background_colors=(
-  ["default"]="49"
-  ["black"]="40"
-  ["red"]="41"
-  ["green"]="42"
-  ["yellow"]="43"
-  ["blue"]="44"
-  ["purple"]="45"
-  ["cyan"]="46"
-  ["white"]="47"
-  ["bright_black"]="100"
-  ["bright_red"]="101"
-  ["bright_green"]="102"
-  ["bright_yellow"]="103"
-  ["bright_blue"]="104"
-  ["bright_purple"]="105"
-  ["bright_cyan"]="106"
-  ["bright_white"]="107"
-)
+background_colors[default]=49
+background_colors[black]=40
+background_colors[red]=41
+background_colors[green]=42
+background_colors[yellow]=43
+background_colors[blue]=44
+background_colors[purple]=45
+background_colors[cyan]=46
+background_colors[white]=47
+background_colors[bright_black]=100
+background_colors[bright_red]=101
+background_colors[bright_green]=102
+background_colors[bright_yellow]=103
+background_colors[bright_blue]=104
+background_colors[bright_purple]=105
+background_colors[bright_cyan]=106
+background_colors[bright_white]=107
 
-emphasis=(
-  ["default"]="0"
-  ["bold"]="1"
-  ["italics"]="3"
-  ["underline"]="4"
-  ["blink"]="5"
-  ["reverse"]="7"
-)
+emphasis[default]=0
+emphasis[bold]=1
+emphasis[italics]=3
+emphasis[underline]=4
+emphasis[blink]=5
+emphasis[reverse]=7
 
 
-
-function log () {
-  # check if message
-  test -n "$1" || {
-    echo;
-    return;
-  }
-
-  #
-  # Extract message
-  #
-  message=$1;
-  shift;
-
-  #
-  # parse options
-  #
-
-  color="default";
-  background_color="default";
-  formatting+=("default");
-  line_break="\n";
-
-  while getopts ":c:k:biunlr" option; do
-    case $option in
-      c)
-        color=$OPTARG;
-        ;;
-      k)
-        background_color=$OPTARG;
-        ;;
-      b)
-        formatting+=("bold")
-        ;;
-      i)
-        formatting+=("italics")
-        ;;
-      u)
-        formatting+=("underline")
-        ;;
-      l)
-        formatting+=("blink")
-        ;;
-      r)
-        formatting+=("reverse")
-        ;;
-      n)
-        line_break="";
-        ;;
-    esac
-  done
-
-  #
-  # build treatment
-  #
-  treatment="${escape}";
-  for format in ${formatting[@]}
-  do
-    treatment="${treatment}${emphasis[$format]};"
-  done
-  # reset formatting array
-  formatting=();
-  treatment="${treatment}${text_colors[$color]};"
-  treatment="${treatment}${background_colors[$background_color]}m"
-
-  printf "${treatment}%s${reset}${line_break}" $message;
+function write-log () {
+	# check if message
+	test -n "$1" || {
+		echo;
+		return;
+	}
+	
+	#
+	# Extract message
+	#
+	message=$1;
+	shift;
+	
+	#
+	# parse options
+	#
+	
+	color="default";
+	background_color="default";
+	formatting+=("default");
+	line_break="\n";
+	TIME_STAMP=`date "+%Y-%m-%d %H:%M:%S"`
+	
+	while getopts ":c:k:biunlr" option; do
+	case $option in
+		c)
+			color=$OPTARG;
+			;;
+		k)
+			background_color=$OPTARG;
+			;;
+		b)
+			formatting+=("bold")
+			;;
+		i)
+			formatting+=("italics")
+			;;
+		u)
+			formatting+=("underline")
+			;;
+		l)
+			formatting+=("blink")
+			;;
+		r)
+			formatting+=("reverse")
+			;;
+		n)
+			line_break="";
+			;;
+	esac
+	done
+	
+	#
+	# build treatment
+	#
+	treatment="${escape}";
+	for format in ${formatting[@]}
+	do
+		treatment="${treatment}${emphasis[$format]};"
+	done
+	# reset formatting array
+	formatting=();
+	treatment="${treatment}${text_colors[$color]};"
+	treatment="${treatment}${background_colors[$background_color]}m"
+	
+	stampedMessage="$TIME_STAMP $message"	
+	printf "${treatment}%s${reset}${line_break}" $stampedMessage;
 }
